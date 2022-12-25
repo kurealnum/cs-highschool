aaed6aaa255472c9f5a4388626560c33101aff39
kureal <116363763+kurealnum@users.noreply.github.com>
2022-12-24 21:25:46 -0500

    Create 3.1.py

---
+++ 2022-2023/3.1.py
@@ -0,0 +1,11 @@
+'''
+Oscar; Asks users a few questions about themselves, responds, and print their responses.
+'''
+
+print("Hello there!")
+fav_color = input("What is your favorite color? ")
+print(f'My favorite color is blue, I think it mixes well with {fav_color}')
+occu_state = input("What state do you live in? ")
+print(f'I live in California, I think that\'s pretty far from {occu_state}')
+current_age = input("How old are you? ")
+print("I am 15 years old! \nGoodbye!")
