084f0bbc4da89c42e590b9abd6b6baadcc86c089
kurealnum <cattledoger@gmail.com>
2023-04-26 18:49:51 -0400

    Completely forgot to commit for a longgg time

--- 2022-2023/Module5/capstone/app.py
+++ 2022-2023/Module5/capstone/app.py
@@ -1,5 +1,5 @@
 #imports
-import csv
+import csv, json
 from flask import Flask, jsonify
 from flask_caching import Cache
 
@@ -20,7 +20,10 @@ with open('greenhouse_gas_inventory_data_data.csv', mode='r') as data:
     for row in data:
         csv_file.append(row)
 
+    csv_file = sorted(csv_file, key=lambda x: x[2])
 
+
+#returns the different types of emissions in this dataset
 @app.route("/types_of_emissions", methods=['GET'])
 @cache.cached(timeout=60)
 def types_of_emissions():
@@ -40,4 +43,35 @@ def types_of_emissions():
 
     return return_statement
 
+
+'''
+returns all of the countries included in this dataset, sorted by amount of pollution
+sort_method allows you to sort countries by ascending or descending
+'''
+@app.route("/countries/<string:sort_method>/<string:type_of_pollution>/<string:year>", methods=["GET"])
+@cache.cached(timeout=60)
+def countries(sort_method, type_of_pollution, year):
+    #deciding which sort method to use
+    if str(sort_method).lower() == "asc":
+        sort_method = False
+    elif str(sort_method).lower() == "desc":
+        sort_method = True
+
+    return_statement = {}
+
+    #get all of the data that we need to sort it, no more and no less
+    for i in csv_file[1:]:
+        if year == i[1] and type_of_pollution == i[3]:
+            return_statement.update({i[0]:float(i[2])})
+
+    #sort the dict by values
+    return_statement = sorted(return_statement.items(), key=lambda x: x[1], reverse=sort_method)
+    return_statement = dict(return_statement)
     
+    #did i struggle trying to find out why this was sorting my keys for 6 hours? yes, yes i did
+    return_statement = json.dumps({"countries": return_statement,
+                                    "type_of_pollution": type_of_pollution}, sort_keys=False)
+   
+    return return_statement
+
+
