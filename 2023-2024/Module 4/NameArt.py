9c4b34a59d1c926a286d5c81f9756ae531497abb
kurealnum <cattledoger@gmail.com>
2023-10-25 10:09:10 -0400

    Very simple function

---
+++ 2023-2024/Module 4/NameArt.py
@@ -0,0 +1,19 @@
+###################################################################
+# Oscar Gaske
+# NameArt
+# Prints the word "Oscar" in a larger format using multiple print functions 
+# 10/25/23
+###################################################################
+
+def main():
+    print(" OOO    SSS    CCC   AA   RRRR  ")
+    print("O   O  S      C     A  A  R   R ")
+    print("O   O   SSS   C     AAAA  RRRR  ")
+    print("O   O      S  C     A  A  R R   ")
+    print(" OOO   SSSS    CCC  A  A  R  RR ")
+    
+main()
+
+
+                            
+                            
